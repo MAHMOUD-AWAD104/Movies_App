f8d13f7bbf2aa00891068475a76959a11dc98d1a
mahmoud awad <mahmoudawadalla104@gmail.com>
2026-03-18 21:43:34 +0200

    edit splash

--- lib/features/splash/presentation/screens/splash_screen.dart
+++ lib/features/splash/presentation/screens/splash_screen.dart
@@ -1,109 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
-// import 'package:go_router/go_router.dart';
-// import 'package:movies_app/core/constants/app_colors.dart';
-// import 'package:movies_app/core/di/injection_container.dart';
-// import 'package:movies_app/core/routes/app_router.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
-
-// class SplashScreen extends StatefulWidget {
-//   const SplashScreen({super.key});
-
-//   @override
-//   State<SplashScreen> createState() => _SplashScreenState();
-// }
-
-// class _SplashScreenState extends State<SplashScreen>
-//     with SingleTickerProviderStateMixin {
-//   late AnimationController _controller;
-//   late Animation<double> _fadeAnimation;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _controller = AnimationController(
-//       vsync: this,
-//       duration: const Duration(milliseconds: 1500),
-//     );
-//     _fadeAnimation = Tween<double>(begin: 0, end: 1).animate(
-//       CurvedAnimation(parent: _controller, curve: Curves.easeIn),
-//     );
-//     _controller.forward();
-//     _navigate();
-//   }
-
-//   Future<void> _navigate() async {
-//     await Future.delayed(const Duration(seconds: 3));
-//     if (!mounted) return;
-
-//     final prefs = sl<SharedPreferences>();
-//     final isOnboarded = prefs.getBool('is_onboarded') ?? false;
-//     final isLoggedIn = prefs.getString('api_key') != null;
-
-//     if (!isOnboarded) {
-//       context.go(AppRoutes.onboarding);
-//     } else if (!isLoggedIn) {
-//       context.go(AppRoutes.login);
-//     } else {
-//       context.go(AppRoutes.home);
-//     }
-//   }
-
-//   @override
-//   void dispose() {
-//     _controller.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: AppColors.background,
-//       body: Center(
-//         child: FadeTransition(
-//           opacity: _fadeAnimation,
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               Container(
-//                 width: 100.w,
-//                 height: 100.w,
-//                 decoration: BoxDecoration(
-//                   color: AppColors.primary,
-//                   borderRadius: BorderRadius.circular(24.r),
-//                 ),
-//                 child: Icon(
-//                   Icons.movie_filter_rounded,
-//                   size: 60.sp,
-//                   color: AppColors.white,
-//                 ),
-//               ),
-//               SizedBox(height: 24.h),
-//               Text(
-//                 'CineYTS',
-//                 style: TextStyle(
-//                   fontSize: 32.sp,
-//                   fontWeight: FontWeight.w700,
-//                   color: AppColors.white,
-//                   letterSpacing: 2,
-//                 ),
-//               ),
-//               SizedBox(height: 8.h),
-//               Text(
-//                 'Your Movie Universe',
-//                 style: TextStyle(
-//                   fontSize: 14.sp,
-//                   color: AppColors.textSecondary,
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
@@ -146,13 +40,13 @@ class _SplashScreenState extends State<SplashScreen>
     if (!mounted) return;
 
     final prefs = sl<SharedPreferences>();
-    final isOnboarded = prefs.getBool('is_onboarded') ?? false;
+    // final isOnboarded = prefs.getBool('is_onboarded') ?? false;
     final isLoggedIn = prefs.getString('api_key') != null;
 
-    if (!isOnboarded) {
+    if (!isLoggedIn) {
       context.go(AppRoutes.onboarding);
-    } else if (!isLoggedIn) {
-      context.go(AppRoutes.login);
+      // } else if (!isLoggedIn) {
+      //   context.go(AppRoutes.login);
     } else {
       context.go(AppRoutes.home);
     }
